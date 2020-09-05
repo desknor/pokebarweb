57842e6b4fe9bc5571745aaa7790facdbbb294d6
desknor <deschdev@gmail.com>
2020-09-05 10:56:08 -0400

    need to fix opacity css later on

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     dropRadius: 19,
     perturbance: 0.007,
   });
-})
+  //magnific popup
+  $('.parent-container').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {enabled: true},
+  });
+});
